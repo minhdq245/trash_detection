5600a0484620aa569540df8242f5d158929805a9
minhdq245 <minhdq2452005@gmail.com>
2025-03-19 04:47:34 +0700

    Update project structure and add Streamlit support

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="waste-detection",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        line.strip()
+        for line in open("requirements.txt").readlines()
+        if not line.startswith("--")
+    ],
+    python_requires=">=3.9,<3.10",
+)
