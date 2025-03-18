6f2cc2ecb2642613034f232e5339dae798bb7ddb
minhdq245 <minhdq2452005@gmail.com>
2025-03-19 05:06:44 +0700

    Update project structure for Streamlit deployment

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         line.strip()
         for line in open("requirements.txt").readlines()
-        if not line.startswith("--")
+        if not line.startswith(("-", "--"))
     ],
-    python_requires=">=3.9,<3.10",
+    python_requires=">=3.10,<3.11",
 )
